d3b46a08fe57eea6aaac2583407e9227d645bd7f
spookyWalrus <zen@Zens-MacBook-Pro.local>
2016-03-10 12:03:56 -0500

    Fuad index page commit

---
+++ fuadJava.js
@@ -0,0 +1,11 @@
+//Fuad Java
+
+// Fuad's javascript coding! 
+
+$(window).on("scroll", function(){
+  if($("body").scrollTop() === 500){
+    $(window).off("scroll");
+    // Do some stuff here ..
+  }
+}
+
